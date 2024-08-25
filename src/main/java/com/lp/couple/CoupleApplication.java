7d7f570062eb4a7d9113e9b3abfb88090ebcdccc
shoon95 <tkdgns5817@gmail.com>
2024-08-25 21:24:12 +0900

    [FIX] 환경 변수 로드 수정
    환경 변수 로드 시 dev, prod에 따라 동적 로드 추가

--- src/main/java/com/lp/couple/CoupleApplication.java
+++ src/main/java/com/lp/couple/CoupleApplication.java
@@ -9,13 +9,24 @@ public class CoupleApplication {
 
     public static void main(String[] args) {
         // .env 파일에서 환경 변수 로드
-        Dotenv dotenv = Dotenv.configure()
-                .directory("./")  // .env 파일의 경로를 명시적으로 설정
-                .ignoreIfMissing()
-                .load();
+        String activeProfile = System.getenv("SPRING_PROFILES_ACTIVE");
 
+        // 값이 없으면 기본값으로 'dev' 프로파일을 사용합니다.
+        if (activeProfile == null || activeProfile.isEmpty()) {
+            activeProfile = "dev";
+            System.setProperty("SPRING_PROFILES_ACTIVE", activeProfile);
+        }
+
+        // 해당 프로파일에 맞는 .env 파일 로드
+        Dotenv dotenv;
+        if ("prod".equals(activeProfile)) {
+            dotenv = Dotenv.configure().filename(".env.prod").load();
+        } else {
+            dotenv = Dotenv.configure().filename(".env.dev").load();
+        }
+
+        // .env 파일에서 읽은 환경 변수를 시스템 속성으로 설정
         dotenv.entries().forEach(entry -> {
-            // 시스템 환경 변수로 설정되어 있지 않은 경우에만 시스템 속성으로 설정
             if (System.getenv(entry.getKey()) == null) {
                 System.setProperty(entry.getKey(), entry.getValue());
             }
