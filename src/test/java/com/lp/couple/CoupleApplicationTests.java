180cd765fe6246c475f80071d3d923bb5ebf650e
shoon95 <tkdgns5817@gmail.com>
2024-08-25 21:15:34 +0900

    [TEST] TEST에 환경 변수 설정 추가
    테스트 코드 초기에 스테이지에 따라 환경 변수 동적 생성 추가

--- src/test/java/com/lp/couple/CoupleApplicationTests.java
+++ src/test/java/com/lp/couple/CoupleApplicationTests.java
@@ -1,11 +1,38 @@
 package com.lp.couple;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoupleApplicationTests {
 
+	@BeforeAll
+	static void setup() {
+		String activeProfile = System.getenv("SPRING_PROFILES_ACTIVE");
+
+		// 값이 없으면 기본값으로 'dev' 프로파일을 사용합니다.
+		if (activeProfile == null || activeProfile.isEmpty()) {
+			activeProfile = "dev";
+			System.setProperty("SPRING_PROFILES_ACTIVE", activeProfile);
+		}
+
+		// 해당 프로파일에 맞는 .env 파일 로드
+		Dotenv dotenv;
+		if ("prod".equals(activeProfile)) {
+			dotenv = Dotenv.configure().filename(".env.prod").load();
+		} else {
+			dotenv = Dotenv.configure().filename(".env.dev").load();
+		}
+
+		// .env 파일에서 읽은 환경 변수를 시스템 속성으로 설정
+		dotenv.entries().forEach(entry -> {
+			if (System.getenv(entry.getKey()) == null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
+		});
+	}
 	@Test
 	void contextLoads() {
 	}
